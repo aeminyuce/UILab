ae34c0220de6e54574a8f688ff20dd7a1a39ce44
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2018-07-11 17:50:48 +0300

    File created.

---
+++ js/portlets.js
@@ -0,0 +1,32 @@
+/*
+ Portlets JS
+ Portlets JS requires Events JS
+*/
+
+/*globals document, events */
+var portletsFnc = function () {
+
+    'use strict';
+
+    // portlet close button
+    events.on(document, 'click', '.portlet-close', function () {
+
+        var parentEl = this.parentElement;
+
+        events.addClass(parentEl, 'portlet-closing ease-close-portlet');
+        setTimeout(function () {
+            parentEl.parentNode.removeChild(parentEl);
+        }, 300);
+
+
+    });
+
+};
+
+/*!loader */
+events.onload(function () {
+
+    'use strict';
+    portletsFnc();
+
+});
