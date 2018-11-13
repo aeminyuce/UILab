c33d1037cac6e653fc6bd9daa532f35657923dbe
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2018-11-13 16:36:53 +0300

    Bug fix.

--- js/sprites.js
+++ js/sprites.js
@@ -35,38 +35,35 @@ var sprites = {
             }
             j += 1;
 
-            if (splitSpriteImages.length === j) {
+            if (selector('#sprites-styles').length === 0) {
+                style = document.createElement('style');
+                style.id = 'sprites-styles';
+                selector('head')[0].appendChild(style);
 
-                if (selector('#sprites-styles').length === 0) {
-                    style = document.createElement('style');
-                    style.id = 'sprites-styles';
-                    selector('head')[0].appendChild(style);
-
-                } else {
-                    style = selector('#sprites-styles');
-                    style.innerHTML = '';
-                }
-
-                splitSpriteImageSizes = sprites.spriteImageSizes.split(',');
-
-                for (i = 0; i < splitSpriteImages.length; i += 1) {
-
-                    sName = splitSpriteImages[i].split('.');
-                    if (splitSpriteImageSizes[i] !== undefined) { sSize = splitSpriteImageSizes[i].split('x'); } else { sSize = ['0', '0']; }
-
-                    if (selector('.sprite[class*="' + sName[0] + '-"]').length > 0) {
-
-                        if (sprites.retinaSprites && window.devicePixelRatio > 1) {
-                            style.innerHTML += '.sprite[class*="' + sName[0] + '-"]{ background-image:url(' + sprites.spritePath + sName[0] + '@2x.' + sName[1] + sprites.spriteVersion + '); background-size:' + sSize[0] + 'px ' + sSize[1] + 'px }';
-
-                        } else if (sprites.loadNoRetinaSprites) {
-                            style.innerHTML += '.sprite[class*="' + splitSpriteImages[i].split('.')[0] + '-"]{ background-image:url(' + sprites.spritePath + splitSpriteImages[i] + sprites.spriteVersion + '); background-size:' + sSize[0] + 'px ' + sSize[1] + 'px }';
-                        }
-
-                    }
-                }
-
+            } else {
+                style = selector('#sprites-styles');
+                style.innerHTML = '';
             }
+
+            splitSpriteImageSizes = sprites.spriteImageSizes.split(',');
+
+            for (i = 0; i < j; i += 1) {
+
+                sName = splitSpriteImages[i].split('.');
+                if (splitSpriteImageSizes[i] !== undefined) { sSize = splitSpriteImageSizes[i].split('x'); } else { sSize = ['0', '0']; }
+
+                if (selector('.sprite[class*="' + sName[0] + '-"]').length > 0) {
+
+                    if (sprites.retinaSprites && window.devicePixelRatio > 1) {
+                        style.innerHTML += '.sprite[class*="' + sName[0] + '-"]{ background-image:url(' + sprites.spritePath + sName[0] + '@2x.' + sName[1] + sprites.spriteVersion + '); background-size:' + sSize[0] + 'px ' + sSize[1] + 'px }';
+
+                    } else if (sprites.loadNoRetinaSprites) {
+                        style.innerHTML += '.sprite[class*="' + splitSpriteImages[i].split('.')[0] + '-"]{ background-image:url(' + sprites.spritePath + splitSpriteImages[i] + sprites.spriteVersion + '); background-size:' + sSize[0] + 'px ' + sSize[1] + 'px }';
+                    }
+
+                }
+            }
+
         }
 
     }
