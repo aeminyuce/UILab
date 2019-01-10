4338a8a7ede88f1de67e0728f5892bdc3d232d92
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-01-10 14:42:56 +0300

    Maps improved and updated.

--- js/svg-map.js
+++ js/svg-map.js
@@ -8,154 +8,62 @@ var svgMap = {};
 (function () {
 
     'use strict';
-    /*globals window, events, selector, setTimeout, navigator, clearTimeout */
-
-    var toolbarTimer;
-
-    function toolbarCloseFnc() {
-
-        var toolbar = selector('.svg-map .toolbar');
-
-        clearTimeout(toolbarTimer);
-        events.removeClass(toolbar, 'open-ease');
-
-        setTimeout(function () {
-            events.removeClass(toolbar, 'open');
-        }, 150);
-
-    }
+    /*globals window, events, selector */
 
     svgMap.Start = function () {
 
-        var map, arr, g, p, i, data, opacity, toolbar, toolbarContent, mobile, current;
-        mobile = false;
-
-        if (navigator.userAgent.toLowerCase().indexOf('mobile') > -1) { // detecting mobile
-            mobile = true;
-        }
+        var map, arr, data, g, opacity;
 
         map = selector('.svg-map');
-        if (map.length !== 1) { return; }
-
-        arr = [];
-        g = selector('.svg-map [data-size]');
-
-        for (i = 0; i < g.length; i += 1) {
-
-            data = g[i].getAttribute('data-size');
-            if (data >= 1) { arr.push(data); }
-
-        }
-
-        arr = arr.sort(function (a, b) { return b - a; });
-
-        events.each(g, function () {
-
-            data = this.getAttribute('data-size');
-
-            if (data > 0) {
-
-                events.addClass(this, 'active');
-
-                opacity = Math.sqrt(data) / Math.sqrt(arr[0]);
-                opacity = opacity.toFixed(2);
-
-                if (opacity > 1) { opacity = 1; }
-                if (opacity < 0.5) { opacity = 0.5; } /* optional */
-
-                selector('path', this)[0].setAttribute('style', 'opacity: ' + opacity + ';');
-
-            }
-
-        });
+        if (map.length === 0) { return; }
 
         arr = [];
 
-        toolbar = selector('.toolbar', map);
-        toolbarContent = selector('span', toolbar)[0];
+        events.each(map, function (i) {
 
-        function showToolbar(e) {
+            arr[i] = [];
+            g = selector('g[data-size]', this);
 
-            clearTimeout(toolbarTimer);
-            toolbar.setAttribute('style', 'top: ' + (e.clientY - (toolbar.offsetHeight + 14)) + 'px; left:' + (e.clientX - (toolbar.offsetWidth / 2)) + 'px;');
+            events.each(g, function () {
 
-            if (e.target.toString() === '[object SVGPathElement]') {
+                data = this.getAttribute('data-size');
+                if (data > 0) { arr[i].push(data); }
 
-                p = e.target.parentNode;
-                toolbarContent.innerHTML = e.target.id + ': ' + p.getAttribute('data-size');
+            });
 
-                if (mobile) {
+            arr[i] = arr[i].sort(function (a, b) { return b - a; });
 
-                    current = selector('.svg-map [data-current]');
-                    if (current.length > 0) {
-                        current[0].removeAttribute('data-current');
-                    }
+            events.each(g, function () {
 
-                    p.setAttribute('data-current', '');
+                data = this.getAttribute('data-size');
+                if (data > 0) {
+
+                    events.addClass(this, 'active');
+
+                    opacity = Math.sqrt(data) / Math.sqrt(arr[i][0]);
+                    opacity = opacity.toFixed(2);
+
+                    if (opacity > 1) { opacity = 1; }
+                    if (opacity < 0.5) { opacity = 0.5; } /* optional */
+
+                    selector('path', this)[0].setAttribute('style', 'opacity: ' + opacity + ';');
 
                 }
 
-            }
+            });
 
-            if (e.target.toString() === '[object SVGSVGElement]') {
-                toolbarCloseFnc();
+            arr[i] = [];
 
-            } else {
-
-                toolbarTimer = setTimeout(function () {
-
-                    events.addClass(toolbar, 'open');
-
-                    setTimeout(function () {
-                        events.addClass(toolbar, 'open-ease');
-                    }, 0);
-
-                }, 300);
-
-            }
-
-            if (mobile) {
-
-                // Events
-                events.on('body', 'touchstart.SVGMaptoolbarCloseFnc', function (e) {
-
-                    if (e.target.toString() !== '[object SVGPathElement]') {
-
-                        events.off('body', 'touchstart.SVGMaptoolbarCloseFnc');
-                        toolbarCloseFnc();
-
-                    }
-
-                });
-
-            }
-
-        }
+        });
 
         // Events
-        if (!mobile) {
-
-            events.on(map, 'mousemove', function (e) { showToolbar(e); });
-            events.on(map, 'mouseleave', toolbarCloseFnc);
-
-            events.on(g, 'click', function () {
-                window.location = this.getAttribute('data-href');
-            });
-
-        } else {
-
-            events.on(map, 'click', function (e) { showToolbar(e); });
-
-            events.on(toolbar, 'click', function () {
-                window.location = selector('.svg-map [data-current]')[0].getAttribute('data-href');
-            });
-
-        }
+        events.on(g, 'click', function () {
+            window.location = this.getAttribute('data-href');
+        });
 
     };
 
     // Loaders
     events.onload(svgMap.Start);
-    events.on(window, 'scroll', toolbarCloseFnc);
 
 }());
