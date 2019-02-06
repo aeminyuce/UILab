1ac5a08ce9c70deaafed22a3b28714491b39eb5a
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-02-06 09:50:02 +0300

    Added indeterminate function.

--- js/forms.js
+++ js/forms.js
@@ -140,6 +140,18 @@ var forms = {};
             selector('span', this.parentElement)[0].innerHTML = this.value;
         });
 
+        // indeterminate
+        events.on(document, 'click', 'input[type="checkbox"].indeterminate', function () {
+
+            if (this.readOnly) {
+                this.checked = this.readOnly = false;
+
+            } else if (!this.checked) {
+                this.readOnly = this.indeterminate = true;
+            }
+
+        });
+
         // form icons
         if (mobile) { // fix: buttons not clicked on form focus at mobile devices
 
