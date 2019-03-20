5633479edfe49858231497339672dfd8cc9f1372
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-03-20 17:27:50 +0300

    Continuing development.

--- js/calendar.js
+++ js/calendar.js
@@ -8,9 +8,9 @@ var calendar = {
     days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 
-    dateFormat : 'mm/dd/yyyy', // mm/dd/yyyy or dd/mm/yyyy
+    dateFormat : 1, // 1: mm/dd/yyyy, 0: dd/mm/yyyy
     startDayofWeek: 1, // 0: Sunday, 1: Monday
-    
+
     fillWeekends: true, // true: fills dark color to weekends' background
 
     prevIcon: 'icon-angle-left', // header's previous button icon
@@ -51,73 +51,108 @@ var calendar = {
     // first loading
     calendar.Start = function () {
 
-        var calendars, createFnc;
+        // get calendar's data-date
+        function getAttr(that, date, newDate) {
 
-        createFnc = function (that, innerDate) {
+            var attr = that.getAttribute('data-date');
+            if (attr !== null && attr !== '') {
 
-            var date, today, getAttr, html, i, j, sysDays, activeDay, days, prevLastDay, firstDay, lastDay;
+                attr = attr.split(',');
+                if (attr.length === 1) { // set only month
 
-            date = new Date();
-            getAttr = that.getAttribute('data-date');
+                    if (!isNaN(Number(attr[0])) && attr[0].length <= 2) {
 
-            if (getAttr !== null && getAttr !== '') {
-
-                getAttr = getAttr.split(',');
-                if (getAttr.length === 1) { // set only month
-
-                    if (!isNaN(Number(getAttr[0])) && getAttr[0].length <= 2) {
-
-                        if (getAttr[0] === 0) { getAttr[0] = 1; }
-                        date.setMonth(getAttr[0] - 1);
+                        if (attr[0] === 0) { attr[0] = 1; }
+                        date.setMonth(attr[0] - 1);
 
                     }
 
-                } else if (getAttr.length === 2) { // set year and month
+                } else if (attr.length === 2) { // set year and month
 
-                    if (!isNaN(Number(getAttr[0])) && getAttr[0].length === 4) {
-                        if (!isNaN(Number(getAttr[1])) && getAttr[1].length <= 2) {
+                    if (!isNaN(Number(attr[0])) && attr[0].length === 4) {
+                        if (!isNaN(Number(attr[1])) && attr[1].length <= 2) {
 
-                            date.setFullYear(getAttr[0]);
+                            date.setFullYear(attr[0]);
 
-                            if (getAttr[1] === '0') { getAttr[1] = 1; }
-                            date.setMonth(getAttr[1] - 1);
+                            if (attr[1] === '0') { attr[1] = 1; }
+                            date.setMonth(attr[1] - 1);
 
                         }
                     }
 
                 }
 
-                if (innerDate === undefined) {
-                    innerDate = getAttr.toString();
+                if (newDate === undefined) {
+                    attr = attr.toString();
+
+                } else {
+                    attr = newDate.toString();
                 }
 
-                if (innerDate === 'prev') {
+                if (attr === 'prev') {
                     date.setMonth(date.getMonth() - 1);
 
-                } else if (innerDate === 'next') {
+                } else if (attr === 'next') {
                     date.setMonth(date.getMonth() + 1);
                 }
 
             }
 
-            that.setAttribute('data-date', date.getFullYear() + ',' + (date.getMonth() + 1));
+        }
 
-            html = '<table class="';
+        // create calendar table
+        function createFnc(that, newDate) {
 
-            if (calendar.fillWeekends) {
-                html += 'fill-weekends ';
+            var date, today, container, html, i, j, sysDays, activeDay, days, prevLastDay, firstDay, lastDay;
+
+            date = new Date();
+
+            // set new date
+            if (newDate !== undefined) {
+
+                if (newDate === 'prev' || newDate === 'next') {
+                    getAttr(that, date, newDate); // get data-date
+
+                } else {
+
+                    newDate = newDate.split(',');
+
+                    date.setFullYear(newDate[0]);
+                    date.setMonth(newDate[1]);
+
+                }
+
+            } else {
+                getAttr(that, date); // get data-date
             }
 
-            html += 'ease-layout ease-slow ease-in-out">' +
+            // set new data-date
+            that.setAttribute('data-date', date.getFullYear() + ',' + (date.getMonth() + 1));
+
+            // create table
+            html = '';
+            container = selector('.calendar-container', that)[0];
+
+            if (container === undefined) {
+                html += '<div class="calendar-container ease-layout ease-slow ease-in-out">';
+            }
+
+            html += '<table';
+
+            if (calendar.fillWeekends) {
+                html += ' class="fill-weekends"';
+            }
+
+            html += '>' +
                         '<caption>' +
-                            '<button class="calendar-prev ease-btn">' +
+                            '<button class="calendar-prev">' +
                                 '<i class="icon icon-md ' + calendar.prevIcon + '"></i>' +
                             '</button>' +
-                            '<span class="calendar-title ease-1st-btn">' +
-                                '<span class="calendar-month">' + calendar.months[date.getMonth()] + '</span>' +
-                                '<span class="calendar-year">' + date.getFullYear() + '</span>' +
+                            '<span class="calendar-title ease-bg">' +
+                                '<button class="calendar-month">' + calendar.months[date.getMonth()] + '</button>' +
+                                '<button class="calendar-year">' + date.getFullYear() + '</button>' +
                             '</span>' +
-                            '<button class="calendar-next ease-btn">' +
+                            '<button class="calendar-next">' +
                                 '<i class="icon icon-md ' + calendar.nextIcon + '"></i>' +
                             '</button>' +
                         '</caption>' +
@@ -127,7 +162,7 @@ var calendar = {
                 html += '<th>' + calendar.days[i] + '</th>';
             }
 
-            html += '<tbody>';
+            html += '</thead><tbody>';
 
             firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
             if (calendar.startDayofWeek === 0) {
@@ -154,7 +189,7 @@ var calendar = {
 
             for (i = 0; i < 6; i += 1) {
 
-                html += '<tr class="ease-2nd-btn">';
+                html += '<tr>';
 
                 for (j = 0; j < 7; j += 1) {
 
@@ -194,20 +229,31 @@ var calendar = {
             }
 
             html += '</tbody>' +
-                    '</thead>' +
                 '</table>';
 
-            html = parser(html);
-            that.innerHTML = html;
-            html = '';
+            if (container === undefined) {
 
+                html += '</div>';
+
+                html = parser(html);
+                that.innerHTML = html;
+
+            } else {
+
+                html = parser(html);
+                container.innerHTML = html;
+
+            }
+
+            html = '';
             events.addClass(that, 'active');
 
-        };
+        }
 
+        // ckeck not loaded calendars
         checkCalendars = function () {
 
-            calendars = selector('.calendar:not(.active)');
+            var calendars = selector('.calendar:not(.active)');
             if (calendars.length > 0) {
 
                 events.each(calendars, function () {
@@ -220,6 +266,8 @@ var calendar = {
         checkCalendars();
 
         // Events
+
+        // calendar navigation
         events.on(document, 'click', '.calendar-prev,.calendar-next', function () {
 
             var that = events.closest(this, '.calendar')[0];
@@ -233,28 +281,108 @@ var calendar = {
 
         });
 
+        // change month and year with panel
         events.on(document, 'click', '.calendar-month,.calendar-year', function () {
 
-            var that, html;
+            var that, date, year, years, month, html, i, panelType, scrollPos;
 
+            date = new Date();
             that = events.closest(this, '.calendar')[0];
-            events.addClass(that, 'top-pane');
 
-            html = '<div class="pane ease-layout ease-slow ease-in-out">';
+            getAttr(that, date);
+            events.addClass(that, 'top-panel');
 
-            if (events.hasClass(this, 'calendar-month')) {
-                html += 'months';
+            // create panel
+            html = '<div class="panel ease-layout ease-slow ease-in-out">' +
+                    '<ul>';
 
-            } else {
-                html += 'years';
+            if (events.hasClass(this, 'calendar-year')) { // years
+
+                panelType = 'year';
+
+                year = date.getFullYear();
+                years = 1920 + (new Date().getFullYear() - 1920) + 100;
+
+                for (i = 1920; i <= years; i += 1) {
+
+                    html += '<li><button ';
+
+                    if (year === i) {
+                        html += 'class="selected" ';
+                    }
+
+                    html += 'name="' + i + '">' + i + '</button></li>';
+
+                }
+
+            } else { // months
+
+                panelType = 'month';
+
+                month = calendar.months[date.getMonth()];
+                for (i = 0; i < calendar.months.length; i += 1) {
+
+                    html += '<li><button ';
+
+                    if (month === calendar.months[i]) {
+                        html += 'class="selected" ';
+                    }
+
+                    html += 'name="' + i + '">' + calendar.months[i] + '</button></li>';
+                }
+
             }
 
-            html += '</div>';
+            html += '</ul>' +
+                '</div>';
+
+            // show panel
             that.insertAdjacentHTML('afterbegin', html);
+            html = '';
+
+            // animate panel
+            setTimeout(function () {
+
+                events.addClass(that, 'show-panel');
+
+                // scroll to active year
+                if (panelType === 'year') {
+
+                    scrollPos = selector('.calendar .panel li button.selected')[0];
+                    scrollPos = scrollPos.getBoundingClientRect();
+
+                    selector('.panel', that)[0].scrollTo(0, scrollPos.y);
+
+                }
+
+            }, 10);
+
+        });
+
+        // close panel
+        events.on(document, 'click', '.calendar .panel li button', function () {
+
+            var that, date;
+
+            date = new Date();
+            that = events.closest(this, '.calendar')[0];
+
+            getAttr(that, date);
+            events.removeClass(that, 'show-panel');
+
+            if (!events.hasClass(this, 'selected')) { // check user selected different date
+
+                if (this.name.length === 4) { // selected year
+                    createFnc(that, this.name + ',' + date.getMonth());
+
+                } else { // selected month
+                    createFnc(that, date.getFullYear() + ',' + this.name);
+                }
+            }
 
             setTimeout(function () {
-                events.addClass(that, 'show-pane');
-            }, 10);
+                that.removeChild(selector('.panel', that)[0]);
+            }, 400);
 
         });
 
