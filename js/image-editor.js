874e54302617b82a084d2bee80a1d9345ebf764d
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-05-07 16:51:31 +0300

    Continuing development.

--- js/image-editor.js
+++ js/image-editor.js
@@ -1,11 +1,13 @@
 /*
  Image Editor JS
- Image Editor JS requires Events JS
+ Image Editor JS requires Events JS, Ajax JS
 */
 
 var imageEditor = {
 
-    limit: 0, // Allowed image length to the list. Ex: 1, 50, 100 etc. 0 is unlimited
+    width: 1024, // default width
+    height: 768, // default height
+
     types: ['jpg', 'jpeg', 'png', 'gif'] // add your allowed file types
 
 };
@@ -13,77 +15,116 @@ var imageEditor = {
 (function () {
 
     'use strict';
-    /*globals document, selector, events, alerts */
+    /*globals document, events, selector, navigator, Image, FileReader */
 
     imageEditor.Start = function () {
 
         // Events
-        events.on(document, 'change', '.editor-list .add-img input', function () {
+        events.on(document, 'change', '.image-editor input[type="file"]', function () {
 
-            var i, ext, list, allowed, added, loadFnc;
+            var i, ext, c, ctx, img, calc, size, allowed, readers, list;
+
+            if (navigator.userAgent.toLowerCase().indexOf('msie 9') > -1) { // IE9 not supported filereader API
+                return;
+            }
 
             if (this.files.length > 0) {
 
-                allowed = [];
-                list = events.closest(this, '.image-editor')[0];
-
                 // check allowed file types
+                allowed = [];
+
                 for (i = 0; i < this.files.length; i += 1) {
 
                     ext = this.files[i].name.split('.')[1].toLowerCase();
-                    ext = ext.match(/^(png|gif|jpeg|jpg)$/g);
-
                     if (ext !== null) {
 
                         ext = ext.toString();
-                        if (imageEditor.types.indexOf(ext) > -1) { allowed.push(this.files[i]); }
+
+                        if (imageEditor.types.indexOf(ext) > -1) {
+                            allowed.push(this.files[i]);
+                        }
 
                     }
 
                 }
 
-                // load images
-                loadFnc = function () {
+                if (allowed.length === 0) { return; } // stop when all file types not allowed
 
-                    console.log('ready!');
+                // load and resize images
+                readers = [];
+                img = [];
+                calc = [];
 
-                };
+                c = document.createElement("canvas");
+                ctx = c.getContext("2d");
 
-                // alert messages
-                added = selector('.img-holder', list).length;
-                if (imageEditor.limit > 0 && added >= imageEditor.limit) { // check allowed limit first
+                list = events.closest(this, '.image-editor')[0];
+                list = selector('.editor-list', list)[0];
 
-                    alerts.dialog({
-                        msg: 'Allowed image limit is <b>' + imageEditor.limit + '</b> and you already added!'
-                    });
+                events.each(allowed, function (i) {
 
-                    return;
+                    readers[i] = new FileReader(); // filereader API
+                    readers[i].onload = function () {
 
-                }
+                        img[i] = new Image();
+                        img[i].src = this.result;
 
-                if (allowed.length === 0) { // all file types not allowed
+                        img[i].onload = function () {
 
-                    alerts.dialog({
-                        msg: 'Selected <b>' + this.files.length + '</b> files not supported!'
-                    });
+                            // resize images to default
+                            if (img[i].width > img[i].height) { // horizontal image
 
-                } else if (this.files.length !== allowed.length) { // some file types are allowed
+                                calc[i] = (img[i].height * imageEditor.width) / img[i].width;
 
-                    alerts.dialog({
-                        msg: 'Added only <b>' + allowed.length + '</b> files.<br><b>' + this.files.length + '</b> files not supported!'
-                    });
+                                c.width = imageEditor.width;
+                                c.height = calc[i];
 
-                    loadFnc();
+                                ctx.drawImage(img[i], 0, 0, imageEditor.width, calc[i]);
 
-                } else { // all file types allowed
+                            } else { // vertical image
 
-                    alerts.dialog({
-                        msg: 'Selected <b>' + allowed.length + '</b> files added!'
-                    });
+                                calc[i] = (img[i].width * imageEditor.height) / img[i].height;
 
-                    loadFnc();
+                                c.width = calc[i];
+                                c.height = imageEditor.height;
 
-                }
+                                ctx.drawImage(img[i], 0, 0, calc[i], imageEditor.height);
+
+                            }
+
+                            // calculate new image file size from new base64
+                            size = c.toDataURL("image/jpeg").length - 'data:image/png;base64,'.length;
+                            size = (4 * Math.ceil(size / 3) * 0.5624896334383812) / 1000;
+
+                            size = size.toFixed(0);
+
+                            // create html
+                            list.insertAdjacentHTML('afterbegin',
+
+                                '<li>' +
+                                    '<span class="check-custom rounded dual-bordered ease-form">' +
+                                        '<input type="checkbox">' +
+                                        '<i class="ease-form-custom"></i>' +
+                                    '</span>' +
+                                    '<span class="name">' + allowed[i].name + '</span>' +
+                                    '<span class="img"><img src="' + c.toDataURL("image/jpeg") + '" alt=""></span>' +
+                                    '<span class="info">' + size + 'kb</span>' +
+                                    '<button class="btn btn-square btn-invisible rounded ease-btn"><i class="icon icon-sm icon-trash"></i></button>' +
+                                '</li>');
+
+                        };
+
+                    };
+
+                    readers[i].readAsDataURL(allowed[i]);
+
+                });
+
+                // empty variables
+                //allowed = [];
+                readers = [];
+                img = [];
+                calc = [];
 
             }
 
