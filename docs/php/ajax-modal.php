aa8d17538b7c802e96b04b32b158c913b2153e35
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-05-10 11:42:16 +0300

    Files created.

---
+++ docs/php/ajax-modal.php
@@ -0,0 +1,20 @@
+<div class="modal-header theme-default ui-dark">
+    <h4>Modal Header</h4>
+    <div class="modal-buttons">
+        <button class="btn circle ui-x-dark ease-btn">Header Button</button>
+    </div>
+</div>
+<div class="modal-container large padding-30">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec tincidunt eros. Vivamus a est id dui ultricies congue vel et tellus. Nam ac velit mi. Donec vitae suscipit enim, nec consectetur nisl. Quisque ac posuere lectus. Morbi quam diam, cursus et metus at, lobortis iaculis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus lectus nibh, non elementum justo facilisis a. Aliquam erat volutpat. Vestibulum auctor et diam eget eleifend. Donec blandit ligula aliquam mi lacinia lacinia. Cras ac bibendum urna. Suspendisse posuere ullamcorper enim, non varius diam mattis vehicula. Donec nec lacinia diam.
+    <img class="img-responsive margin-30-v rounded automargin block shadow-lg" src="images/image_05.jpg" style="max-width: 700px" alt="">
+    Pellentesque congue eget elit vitae rutrum. Morbi bibendum dignissim convallis. Etiam lacinia quam et ipsum convallis, a congue metus sagittis. Nunc posuere iaculis nulla, nec rutrum neque condimentum at. Donec nec elementum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor libero sem, sed bibendum elit viverra vitae.
+    <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec tincidunt eros. Vivamus a est id dui ultricies congue vel et tellus. Nam ac velit mi. Donec vitae suscipit enim, nec consectetur nisl. Quisque ac posuere lectus. Morbi quam diam, cursus et metus at, lobortis iaculis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus lectus nibh, non elementum justo facilisis a. Aliquam erat volutpat. Vestibulum auctor et diam eget eleifend. Donec blandit ligula aliquam mi lacinia lacinia. Cras ac bibendum urna. Suspendisse posuere ullamcorper enim, non varius diam mattis vehicula. Donec nec lacinia diam.
+</div>
+<div class="modal-footer ease-1st-btn">
+    <div class="modal-buttons">
+        <button type="button" class="btn padding-30-h btn-invisible dual-bordered circle" onclick="modal.close(function () { alert('Modal closed!'); });">Callback</button>
+        <button type="button" class="btn padding-30-h btn-invisible dual-bordered circle" onclick="modal.close();">Cancel</button>
+        <button type="submit" class="btn padding-30-h circle theme-green ui-dark" onclick="modal.close();">Accept</button>
+    </div>
+</div>
