2954c3b41ca9f186f3d6edf8cfba8fd9d0ef124c
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-05-10 11:41:39 +0300

    Updated ajax properties call.

--- js/ajax.js
+++ js/ajax.js
@@ -4,87 +4,98 @@
 
  Example:
 
-    ajax('GET', 'ajaxtest.php?name=value&name=value', function (response, status) {
+    ajax({
+        type : 'POST',
+        url : 'yourfile.php',
+        data: 'name=value&name=value',
+        callback: function (status, response) {
 
-        var target = selector('.ajaxTarget');
-        if (target.length > 0) {
+            var target = selector('.ajaxTarget');
+            if (target.length > 0) {
 
-            if (status === 'success') {
-                target[0].innerHTML = response;
+                if (status === 'success') {
+                    target[0].innerHTML = response;
 
-            } else {
-                throw new Error('Ajax Alert: Source not loaded!');
+                } else {
+                    throw new Error('Ajax Alert: Source not loaded!');
+                }
+
             }
 
         }
-
     });
 
  JSON Example:
 
     var myJSONRequests = [];
 
-    ajax('GET', 'ajaxtest.php?name=value&name=value', function (response, status, xhr) {
+    ajax({
+        type : 'GET',
+        url : 'yourfile.php',
+        data: 'name=value&name=value',
+        callback: function (status, response, xhr) {
 
-        var i, n, target;
+            var i, n, target;
 
-        target = selector('.ajaxTarget');
-        if (target.length > 0) {
+            target = selector('.ajaxTarget');
+            if (target.length > 0) {
 
-            // abort still processing previous json requests (optional)
-            for (n = 0; n < myJSONRequests.length; n += 1) {
+                // abort still processing previous json requests (optional)
+                for (n = 0; n < myJSONRequests.length; n += 1) {
 
-                myJSONRequests[n].abort();
-                myJSONRequests.splice(n, 1);
+                    myJSONRequests[n].abort();
+                    myJSONRequests.splice(n, 1);
 
-            }
+                }
 
-            myJSONRequests.push(xhr);
+                myJSONRequests.push(xhr);
 
-            if (status === 'success') {
+                if (status === 'success') {
 
-                myJSONRequests = []; // (optional)
+                    myJSONRequests = []; // (optional)
 
-                response = JSON.parse(response);
-                if (response.length !== 'undefined') {
+                    response = JSON.parse(response);
+                    if (response.length !== 'undefined') {
 
-                    for (i = 0; i < response.length; i += 1) {
-                        if (response[i] !== null) {
-                            console.log(response[i].yourJSONvalue);
+                        for (i = 0; i < response.length; i += 1) {
+                            if (response[i] !== null) {
+                                console.log(response[i].yourJSONvalue);
+                            }
                         }
+
+                    } else {
+                        throw new Error('Ajax Alert: Source is not in correct JSON format!');
                     }
 
-                } else {
-                    throw new Error('Ajax Alert: Source is not in correct JSON format!');
                 }
 
             }
 
         }
-
     });
 
 */
 
 /*globals document, events, XMLHttpRequest */
-var ajax = function (method, target, callback) {
+var ajax = function (set) { // type, url, data, callback
 
     'use strict';
-    var newMethod, newTarget;
 
-    if (typeof method !== 'string' || method === undefined) {
-        return;
-    }
-    newMethod = method;
+    if (set.url === undefined) { return; }
 
-    if (typeof target !== 'string' || target === undefined) {
-        return;
+    if (set.type === undefined || set.type === '') {
+        set.type = 'POST';
     }
-    newTarget = target;
 
     ajax.request = new XMLHttpRequest();
-    ajax.request.open(newMethod, newTarget, true);
-    ajax.request.send();
+    ajax.request.open(set.type, set.url, true);
+
+    if (set.data !== '' && set.data !== undefined) {
+        ajax.request.send(set.data);
+
+    } else {
+        ajax.request.send();
+    }
 
     ajax.request.onload = function () {
 
@@ -94,7 +105,7 @@ var ajax = function (method, target, callback) {
 
         if (ajax.request.status >= 200 && ajax.request.status < 400) {
 
-            callback(ajax.request.responseText, 'success', ajax.request);
+            set.callback('success', ajax.request.responseText, ajax.request);
 
             // get list of real classnames
             ajax.classNames = ajax.request.responseText.match(/\sclass=\"+[\w\s\d\-\_\=]+\"[\s\>]/g);
@@ -109,10 +120,12 @@ var ajax = function (method, target, callback) {
 
             }
 
-        } else { callback('', 'error', ajax.request); }
+        } else { set.callback('error', '', ajax.request); }
 
     };
 
-    ajax.request.onerror = function () { callback('', 'error', ajax.request); };
+    ajax.request.onerror = function () {
+        set.callback('error', '', ajax.request);
+    };
 
 };
