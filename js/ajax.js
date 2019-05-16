65e49c502ab58ed7235f4b40370c0655940a4db4
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-05-16 16:40:42 +0300

    Added beforesend callback function.

--- js/ajax.js
+++ js/ajax.js
@@ -30,6 +30,19 @@
         type : 'GET',
         url : 'yourfile.php',
         data: 'name=value&name=value',
+        beforesend: function (xhr) {
+
+            // abort still processing previous json requests (optional)
+            for (n = 0; n < myJSONRequests.length; n += 1) {
+
+                myJSONRequests[n].abort();
+                myJSONRequests.splice(n, 1);
+
+            }
+
+            myJSONRequests.push(xhr);
+
+        },
         callback: function (status, response, xhr) {
 
             var i, n, target;
@@ -37,19 +50,7 @@
             target = selector('.ajaxTarget');
             if (target.length > 0) {
 
-                // abort still processing previous json requests (optional)
-                for (n = 0; n < myJSONRequests.length; n += 1) {
-
-                    myJSONRequests[n].abort();
-                    myJSONRequests.splice(n, 1);
-
-                }
-
-                myJSONRequests.push(xhr);
-
                 if (status === 'success') {
-
-                    myJSONRequests = []; // (optional)
 
                     response = JSON.parse(response);
                     if (response.length !== 'undefined') {
@@ -82,45 +83,52 @@ var ajax = function (set) { // type, url, data, callback
         set.type = 'POST';
     }
 
-    var i = new Date().getTime();
+    var i = ajax.request.length + 1;
 
     ajax.request[i] = new XMLHttpRequest();
     ajax.request[i].open(set.type, set.url, true);
 
+    // beforesend
+    if (set.beforesend !== undefined) {
+        set.beforesend(ajax.request[i]);
+    }
+
+    // check params in url
     if (set.data !== '' && set.data !== undefined) {
         ajax.request[i].send(set.data);
 
-    } else {
-        ajax.request[i].send();
-    }
+    } else { ajax.request[i].send(); }
 
+    // xhr loading
     ajax.request[i].onload = function () {
 
-        function uniqueClassNames(value, index, self) {
-            return self.indexOf(value) === index;
-        }
-
-        if (ajax.request[i].status >= 200 && ajax.request[i].status < 400) {
+        if (ajax.request[i].readyState === 4 && ajax.request[i].status === 200) {
 
             set.callback('success', ajax.request[i].responseText, ajax.request[i]);
 
             // get list of real classnames
             ajax.classNames = ajax.request[i].responseText.match(/\sclass=\"+[\w\s\d\-\_\=]+\"[\s\>]/g);
-
             if (ajax.classNames !== null) {
 
-                ajax.classNames = ajax.classNames.toString().match(/"+[\w\s\d\-\_\=]+"/g).toString().replace(/\"/g, '').replace(/,/g, ' ').split(' ');
-                ajax.classNames = ajax.classNames.filter(uniqueClassNames);
+                ajax.classNames = ajax.classNames.toString().match(/"+[\w\s\d\-\_\=]+"/g);
+                ajax.classNames = ajax.classNames.toString().replace(/\"/g, '').replace(/,/g, ' ').split(' ');
+
+                ajax.classNames = ajax.classNames.filter(function (value, index, self) {
+                    return self.indexOf(value) === index;
+                });
 
                 // ajax callbacks
                 events.trigger(document, 'ajaxCallbacks'); // set custom event
 
             }
 
-        } else { set.callback('error', '', ajax.request[i]); }
+        } else { // error
+            set.callback('error', '', ajax.request[i]);
+        }
 
     };
 
+    // error
     ajax.request[i].onerror = function () {
         set.callback('error', '', ajax.request[i]);
     };
