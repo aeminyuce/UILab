0db6528bf79d08a1419122765c7cb60a42b0dcd3
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-07-03 17:25:56 +0300

    Pie charts completed.

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -8,94 +8,112 @@ var pieChart = {};
 (function () {
 
     'use strict';
-    /*globals document, selector, events, ajax */
+    /*globals window, document, selector, events, ajax */
 
     pieChart.Start = function () {
 
-        var chart, elems, i, id, deg, styles, css, arr, fill, percent, html;
+        var chart, elems, i, id, deg, loadFnc, head, styles, css, arr, fill, percent, html;
 
-        chart = selector('.pie-chart:not(.loaded)');
+        chart = selector('.pie-chart');
         if (chart.length < 1) { return; }
 
         arr = [];
-
-        selector('head')[0].insertAdjacentHTML('beforeEnd', '<style id="pieChartStyles"></style>');
         styles = selector('#pieChartStyles')[0];
+
+        if (styles === undefined) {
+
+            head = document.getElementsByTagName('head')[0];
+
+            styles = document.createElement('style');
+            styles.id = 'pieChartStyles';
+
+            head.appendChild(styles);
+            styles = selector('#pieChartStyles')[0];
+
+        } else { styles.innerHTML = ''; }
+
+        loadFnc = function (that) {
+
+            percent = that.getAttribute('data-percent');
+
+            if (percent === null && percent === '') {
+                percent = 0;
+            }
+
+            deg = (percent * 360) / 100;
+
+            id = Math.random().toString().split('.')[1];
+            id = id.substring(id.length - 4, id.length);
+
+            id = 'pieId' + id;
+            that.id = id;
+
+            css = '';
+
+            fill = that.getAttribute('data-fill');
+
+            if (fill !== null && fill !== '') {
+                css += '#' + id + ' { color: ' + fill + '}\n';
+            }
+
+            if (deg > 180) {
+
+                html = '<span class="l"></span>' +
+                    '<span class="r"></span>';
+
+                css += '#' + id + ' .l:before { -ms-transform: rotate(' + (deg - 180) + 'deg); transform: rotate(' + (deg - 180) + 'deg); }\n' +
+                    '#' + id + ' .r:before { -ms-transform: rotate(180deg); transform: rotate(180deg); }';
+
+            } else {
+
+                html = '<span class="r"></span>';
+                css += '#' + id + ' .r:before { -ms-transform: rotate(' + deg + 'deg); transform: rotate(' + deg + 'deg); }';
+
+            }
+
+            that.insertAdjacentHTML('beforeEnd', html);
+
+            if (elems.length > 0) {
+
+                i = Array.prototype.slice.call(elems).indexOf(that);
+                if (i > 0) {
+
+                    css += '#' + id + ' { -ms-transform: rotate(' + arr[i - 1] + 'deg); transform: rotate(' + arr[i - 1] + 'deg); }';
+                    arr[i] = arr[i - 1] + deg;
+
+                } else { arr[i] = deg; }
+
+            }
+
+            styles.insertAdjacentHTML('beforeEnd', css);
+
+        };
 
         events.each(chart, function () {
 
-            events.addClass(this, 'loaded');
-
             elems = selector('li', this);
-            events.each(elems, function () {
+            this.style.height = this.offsetWidth + 'px';
 
-                percent = this.getAttribute('data-percent');
-                if (percent === null && percent === '') {
-                    percent = 0;
-                }
-
-                deg = (percent * 360) / 100;
-                deg = deg.toString().split('.');
-
-                if (deg.length > 1) {
-
-                    deg[1] = deg[1].substring(deg[1].length - 4, deg[1].length);
-                    deg = deg[0] + '.' + deg[1];
-
-                }
-
-                id = Math.random().toString().split('.')[1];
-                id = id.substring(id.length - 4, id.length);
-
-                id = 'pieId' + id;
-                this.id = id;
-
-                css = '';
-
-                fill = this.getAttribute('data-fill');
-                if (fill !== null && fill !== '') {
-                    css += '#' + id + ' { color: ' + fill + '}\n';
-                }
-
-                if (deg > 180) {
-
-                    html = '<span class="l ease-pie"></span>' +
-                        '<span class="r ease-pie"></span>';
-
-                    css += '#' + id + ' .l:before { -ms-transform: rotate(' + (deg - 180) + 'deg); transform: rotate(' + (deg - 180) + 'deg); }\n' +
-                        '#' + id + ' .r:before { -ms-transform: rotate(180deg); transform: rotate(180deg); }';
-
-                } else {
-
-                    html = '<span class="r ease-pie"></span>';
-                    css += '#' + id + ' .r:before { -ms-transform: rotate(' + deg + 'deg); transform: rotate(' + deg + 'deg); }';
-
-                }
-
-                this.insertAdjacentHTML('beforeEnd', html);
-
-                if (elems.length > 0) {
-
-                    i = Array.prototype.slice.call(elems).indexOf(this);
-                    if (i > 0) {
-
-                        styles = '#' + id + ' { -ms-transform: rotate(' + arr[i - 1] + 'deg); transform: rotate(' + arr[i - 1] + 'deg); }';
-                        arr[i] = arr[i - 1] + deg;
-
-                    } else { arr[i] = deg; }
-
-                }
-
-                styles.insertAdjacentHTML('beforeEnd', css);
-
-            });
+            events.each(elems, function () { loadFnc(this); });
 
         });
 
     };
 
+    function pieChartResizer() {
+
+        var chart = selector('.pie-chart');
+        if (chart.length < 1) { return; }
+
+        events.each(chart, function () {
+            this.style.height = this.offsetWidth + 'px';
+        });
+
+    }
+
     // Loaders
     events.onload(pieChart.Start);
+    events.on(window, 'resize', pieChartResizer);
 
     // ajax callback loader: requires Ajax JS
     events.on(document, 'ajaxCallbacks', function () {
