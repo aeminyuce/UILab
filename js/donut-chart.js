4a333c731090a0214fb0400654424679dcd8500e
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-07-04 16:16:20 +0300

    Added title messages.

--- js/donut-chart.js
+++ js/donut-chart.js
@@ -10,83 +10,135 @@ var donutChart = {};
     'use strict';
     /*globals document, selector, events, setTimeout, ajax */
 
+    var loadCharts;
+
     donutChart.Start = function () {
 
-        var chart, circles, percent, dashoffset, angle, arrPercent, arrAngle;
+        loadCharts = function () {
 
-        arrPercent = [];
-        arrAngle = [];
+            var chart, circles, percent, dashoffset, angle, arrPercent, arrAngle;
 
-        chart = selector('.donut-chart');
-        if (chart.length > 0) {
+            arrPercent = [];
+            arrAngle = [];
 
-            events.each(chart, function () {
+            chart = selector('.donut-chart');
+            if (chart.length > 0) {
 
-                circles = selector('circle:not(.bg)', this);
+                events.each(chart, function () {
 
-                if (circles.length > 1) {
-                    events.addClass(this, 'multiple');
-                }
+                    circles = selector('circle:not(.bg)', this);
 
-                events.each(circles, function (index) {
-
-                    var that = this;
-
-                    percent = that.getAttribute('data-percent');
-                    arrPercent.push(percent);
-
-                    dashoffset = Math.floor(440 - (percent * 4.4));
-                    if (dashoffset < 0) { dashoffset = 0; }
-
-                    that.setAttribute('stroke-dashoffset', dashoffset);
-
-                    if (index === 0) {
-                        events.addClass(that, 'active');
+                    if (circles.length > 1) {
+                        events.addClass(this, 'multiple');
                     }
 
-                    if (index > 0) {
+                    events.each(circles, function (index) {
 
-                        angle = Math.floor(arrAngle[index - 1] + ((arrPercent[index - 1]) * 3.6));
-                        arrAngle.push(angle);
+                        var that = this;
 
-                        /*
-                            In svg elements CSS only transforms NOT cross browser! All IE browsers not supported!
+                        percent = that.getAttribute('data-percent');
+                        arrPercent.push(percent);
 
-                            How to make transform-origin: 50% 50%?
-                            rotate supported inside this: // rotate(angle, centerWidth, centerHeight)
+                        dashoffset = Math.floor(440 - (percent * 4.4));
+                        if (dashoffset < 0) { dashoffset = 0; }
 
-                            Ex:
-                            <rect x='65' y='65' width='150' height='80' transform='rotate(45 140 105)' />
+                        that.setAttribute('stroke-dashoffset', dashoffset);
+                        if (index > 0) {
 
-                            angle: 45
-                            centerWidth: 140 = 65 + 150/2
-                            centerHeight: 105 = 65 +  80/2
+                            angle = Math.floor(arrAngle[index - 1] + ((arrPercent[index - 1]) * 3.6));
+                            arrAngle.push(angle);
 
-                        */
+                            /*
+                                In svg elements CSS only transforms NOT cross browser! All IE browsers not supported!
 
-                        that.setAttribute('transform', 'rotate(' + angle + ' 80 80)');
+                                How to make transform-origin: 50% 50%?
+                                rotate supported inside this: // rotate(angle, centerWidth, centerHeight)
 
-                    } else { arrAngle.push(0); }
+                                Ex:
+                                <rect x='65' y='65' width='150' height='80' transform='rotate(45 140 105)' />
 
-                    if (events.hasClass(document, 'no-transitions-all animate-stop-all')) {
-                        events.addClass(that, 'loaded');
+                                angle: 45
+                                centerWidth: 140 = 65 + 150/2
+                                centerHeight: 105 = 65 +  80/2
 
-                    } else { // wait for page preload
+                            */
 
-                        setTimeout(function () {
+                            that.setAttribute('transform', 'rotate(' + angle + ' 80 80)');
+
+                        } else { arrAngle.push(0); }
+
+                        if (events.hasClass(document, 'no-transitions-all animate-stop-all')) {
+
                             events.addClass(that, 'loaded');
-                        }, 300);
+                            setTimeout(function () {
+                                that.style.transitionDuration = '.15s';
+                            }, 2000);
 
-                    }
+                        } else { // wait for page preload
+
+                            setTimeout(function () {
+
+                                events.addClass(that, 'loaded');
+                                setTimeout(function () {
+                                    that.style.transitionDuration = '.15s';
+                                }, 2000);
+
+                            }, 300);
+
+                        }
+
+                    });
+
+                    arrPercent = [];
+                    arrAngle = [];
 
                 });
 
-                arrPercent = [];
-                arrAngle = [];
+            }
 
-            });
+        };
+        loadCharts();
 
-        }
+        // Events
+        events.on(document, 'mouseenter touchend', '.donut-chart circle[data-title]', function () {
+
+            var that, svg, circle, chart, msg, title;
+
+            that = this;
+            chart = events.closest(that, '.donut-chart')[0];
+
+            if (events.hasClass(chart, 'multiple')) { // bring to front hovered circle element. z-index not working with SVG elements!
+
+                svg = that.parentNode;
+                svg.insertBefore(that, svg.lastChild);
+
+            }
+
+            // show titles
+            circle = selector('circle', chart);
+
+            setTimeout(function () {
+
+                events.removeClass(circle, 'selected');
+                events.addClass(that, 'selected');
+
+            }, 0);
+
+            msg = selector('strong', chart)[0];
+            if (msg === undefined) {
+
+                chart.insertAdjacentHTML('beforeEnd', '<strong></strong>');
+                msg = selector('strong', chart)[0];
+
+            }
+
+            title = that.getAttribute('data-title');
+
+            if (title !== null && title !== '') {
+                msg.textContent = title;
+            }
+
+        });
 
     };
 
@@ -95,7 +147,7 @@ var donutChart = {};
 
     // ajax callback loader: requires Ajax JS
     events.on(document, 'ajaxCallbacks', function () {
-        if (ajax.classNames.indexOf('donut-chart') > -1) { donutChart.Start(); }
+        if (ajax.classNames.indexOf('donut-chart') > -1) { loadCharts(); }
     });
 
 }());
