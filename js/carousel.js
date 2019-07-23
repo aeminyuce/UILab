79962de6e82c4f3dd3293d0b5615bcf67b00fe7c
Ahmet Emin Yüce <yuce_ahmet@hotmail.com>
2019-07-23 15:21:10 +0300

    Files merged.

--- js/carousel.js
+++ js/carousel.js
@@ -674,6 +674,37 @@ var carousel = {
 
             });
 
+            // carousel gallery
+            events.on('.carousel-gallery .thumbs .img', 'click', function () {
+
+                var parent, detail, target, thumbs, index, newImg;
+
+                parent = events.closest(this, '.carousel-gallery');
+
+                detail = selector('.detail', parent[0]);
+                target = selector('img', detail);
+                thumbs = selector('.thumbs .img', parent[0]);
+
+                index = Array.prototype.slice.call(thumbs).indexOf(this);
+                target.setAttribute('data-count', index);
+
+                events.addClass(detail, 'detail-loader');
+
+                newImg = new Image();
+                newImg.src = this.getAttribute('data-href');
+
+                newImg.addEventListener('load', function () {
+
+                    target.src = newImg.src;
+                    events.removeClass(detail, 'detail-loader');
+
+                });
+
+                events.removeClass(thumbs, 'selected');
+                events.addClass(this, 'selected');
+
+            });
+
         }
 
     };
