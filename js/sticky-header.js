cdfe730c2b81d67643683171479fea862b3f4b62
ahmeteminyuce <ahmeteminyuce@users.noreply.github.com>
2019-10-18 11:33:21 +0300

    Added responsive class names.

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -12,23 +12,16 @@ var stickyHeader = {};
     /*globals window, document, events, selector */
     stickyHeader.Start = function () {
 
-        var header, stickyStatic;
-        header = selector('header');
+        var header, screenW, clearStatic, stickyStatic;
 
+        header = selector('header');
         if (header.length > 0) {
 
+            header = header[0];
+            screenW = window.innerWidth;
+
             stickyStatic = events.hasClass(header, 'sticky-static');
-
-            if (window.pageYOffset > header[0].offsetTop) {
-
-                if (!stickyStatic) {
-                    selector('body')[0].style.paddingTop = header[0].offsetHeight + 'px';
-                }
-
-                events.addClass(document, 'sticky-header');
-                events.addClass(header, 'sticky');
-
-            } else {
+            clearStatic = function () {
 
                 if (!stickyStatic) {
                     selector('body')[0].style.paddingTop = '0';
@@ -37,7 +30,25 @@ var stickyHeader = {};
                 events.removeClass(document, 'sticky-header');
                 events.removeClass(header, 'sticky');
 
-            }
+            };
+
+            if (window.pageYOffset > header.offsetTop) {
+
+                if (events.hasClass(header, 'sticky-xs') && screenW > 480) { clearStatic(); return; }
+                if (events.hasClass(header, 'sticky-sm') && screenW > 767) { clearStatic(); return; }
+                if (events.hasClass(header, 'sticky-md') && screenW > 959) { clearStatic(); return; }
+
+                if (events.hasClass(header, 'sticky-lg') && screenW < 1200) { clearStatic(); return; }
+                if (events.hasClass(header, 'sticky-xl') && screenW < 1680) { clearStatic(); return; }
+                
+                if (!stickyStatic) {
+                    selector('body')[0].style.paddingTop = header.offsetHeight + 'px';
+                }
+
+                events.addClass(document, 'sticky-header');
+                events.addClass(header, 'sticky');
+
+            } else { clearStatic(); }
 
         }
 
