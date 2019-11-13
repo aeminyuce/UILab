8ff63bec6cc7abc481c6d8a6e51eda2f028846ae
ahmeteminyuce <ahmeteminyuce@users.noreply.github.com>
2019-11-13 16:40:01 +0300

    Documentation updates.

---
+++ docs/php/ajax-products.php
@@ -0,0 +1,209 @@
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_02.png,img/product_04.png"
+                    data-srcset="img/product_02@2x.png,img/product_04@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">Purple</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Bluetooth Headphones</span>
+        <b class="xx-large">$79.95</b> <b class="x-dark overline">$99.99</b>
+        <span class="block">
+            <i class="icon icon-sm margin-3-r icon-tag"></i> 20% off
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-5 dual-bordered" style="border-color: #d22fdf;"></li>
+            <li class="circle padding-5 dual-bordered" style="border-color: #dedede;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">3.9</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 9
+            </span>
+        </span>
+    </a>
+</div>
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_03.png,img/product_05.png"
+                    data-srcset="img/product_03@2x.png,img/product_05@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">Blue</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Wireless Headphones</span>
+        <b class="xx-large">$149.99</b>
+        <span class="block">
+            <i class="icon icon-sm icon-finish margin-2-r"></i> Fast Delivery
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-5 dual-bordered" style="border-color: #248ddd;"></li>
+            <li class="circle padding-5 dual-bordered" style="border-color: #3b3b3b;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">4.3</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 11
+            </span>
+        </span>
+    </a>
+</div>
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_04.png"
+                    data-srcset="img/product_04@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">White</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Wired Headphones</span>
+        <b class="xx-large">$89.95</b> <b class="x-dark overline">$119.99</b>
+        <span class="block">
+            <i class="icon icon-sm margin-3-r icon-tag"></i> 20% off
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-5 dual-bordered" style="border-color: #dedede;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">4.9</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 32
+            </span>
+        </span>
+    </a>
+</div>
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_05.png"
+                    data-srcset="img/product_05@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">Black</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Wired Headphones</span>
+        <b class="xx-large">$89.95</b> <b class="x-dark overline">$119.99</b>
+        <span class="block">
+            <i class="icon icon-sm margin-3-r icon-tag"></i> 20% off
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-10 bordered" style="background: #3b3b3b;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">4.6</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 44
+            </span>
+        </span>
+    </a>
+</div>
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_01.png,img/product_02.png,img/product_03.png"
+                    data-srcset="img/product_01@2x.png,img/product_02@2x.png,img/product_03@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">Red</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Wireless Headphones</span>
+        <b class="xx-large">$164.98</b>
+        <span class="block">
+            <i class="icon icon-sm icon-finish margin-2-r"></i> Fast Delivery
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-5 dual-bordered" style="border-color: #ee2329;"></li>
+            <li class="circle padding-5 dual-bordered" style="border-color: #d22fdf;"></li>
+            <li class="circle padding-5 dual-bordered" style="border-color: #248ddd;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">4.8</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 24
+            </span>
+        </span>
+    </a>
+</div>
+<div class="col-4 col-sm-6 xs-responsive">
+    <a class="full-height padding-15 no-padding-t portlet rounded shadow-lg hover-shadow hover-up ease-layout" href="#">
+        <span class="photo-slider margin-20-b">
+            <i class="slider-loader icon icon-xl icon-loader"></i>
+            <img src="img/product_blank.gif"
+                    data-src="img/product_02.png,img/product_04.png"
+                    data-srcset="img/product_02@2x.png,img/product_04@2x.png"
+                alt="">
+            <button title="Prev" class="slide-left rounded ease-layout">
+                <i class="icon icon-md icon-angle-left"></i>
+            </button>
+            <button title="Next" class="slide-right rounded ease-layout">
+                <i class="icon icon-md icon-angle-right"></i>
+            </button>
+            <span class="slider-nav"></span>
+        </span>
+        <span class="x-large">Purple</span>
+        <span class="x-dark block margin-10-v">beats by dr. dre Studio 3 Bluetooth Headphones</span>
+        <b class="xx-large">$79.95</b> <b class="x-dark overline">$99.99</b>
+        <span class="block">
+            <i class="icon icon-sm margin-3-r icon-tag"></i> 20% off
+        </span>
+        <ul class="list-inline margin-15-v right">
+            <li class="circle padding-5 dual-bordered" style="border-color: #d22fdf;"></li>
+            <li class="circle padding-5 dual-bordered" style="border-color: #dedede;"></li>
+        </ul>
+        <span class="left padding-15-v margin-2-t">
+            <i class="icon icon-sm icon-favorite-fill font-yellow margin-4-b"></i>
+            <b class="x-dark">3.9</b>
+            <span class="dark">
+                <i class="icon icon-sm icon-message margin-3-b margin-5-l"></i> 9
+            </span>
+        </span>
+    </a>
+</div>
