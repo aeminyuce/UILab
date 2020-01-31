2b2d7c93ebfcee5fbc12e58f547569774dd33a0d
A. Emin Yuce <yuce_ahmet@hotmail.com>
2020-01-31 11:20:03 +0300

    Merged toggle password.

--- js/forms.js
+++ js/forms.js
@@ -148,6 +148,29 @@ var forms = {};
 
         });
 
+        // toggle password
+        events.on(document, 'click touchend', '.toggle-password', function () {
+
+            var t, p, id = this.getAttribute('data-id');
+
+            if (id === null) {
+
+                p = events.closest(this, '.text');
+                if (p.length === 1) {
+                    t = selector('input', p);
+                }
+
+            } else { t = selector('#' + id)[0]; }
+
+            if (t.getAttribute('type') === 'password') {
+                t.setAttribute('type', 'text');
+
+            } else {
+                t.setAttribute('type', 'password');
+            }
+
+        });
+
         // form icons
         if (useragents.mobile) { // fix: buttons not clicked on form focus at mobile devices
 
