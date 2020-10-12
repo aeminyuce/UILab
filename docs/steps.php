176feb44847fa6d7ccd9b9c40077ef4057fa0d04
A. Emin Yuce <yuce_ahmet@hotmail.com>
2020-10-12 11:09:49 +0300

    Bug fix for steps in card.

--- docs/steps.php
+++ docs/steps.php
@@ -106,6 +106,111 @@
                     </ul>
                 </div>
 
+                <div class="card padding-30 round shadow-lg">
+
+                    <div class="padding-30-b">
+                        <ul class="steps-bar">
+                            <li class="active"></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <span class="sp-15"></span>
+                        <ul class="steps-bar theme-yellow ui-text">
+                            <li></li>
+                            <li class="active"></li>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div class="padding-30-v">
+                        <ul class="steps-bar">
+                            <li class="active">Step Name Here</li>
+                            <li>Step Name Here</li>
+                            <li>Step Name Here</li>
+                        </ul>
+                        <span class="sp-15"></span>
+                        <ul class="steps-bar theme-yellow ui-text">
+                            <li>Step Name Here</li>
+                            <li class="active">Step Name Here</li>
+                            <li>Step Name Here</li>
+                        </ul>
+                    </div>
+
+                    <div class="padding-30-v">
+                        <ul class="steps-bar">
+                            <li>Step Name Here</li>
+                            <li>Step Name Here</li>
+                            <li class="active">Step Name Here</li>
+                            <li>Step Name Here</li>
+                            <li>Step Name Here</li>
+                        </ul>
+                        <span class="sp-15"></span>
+                        <ul class="steps-bar theme-orange ui-text">
+                            <li>
+                                <span data-tooltip title="Completed"></span>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <span data-tooltip title="Completed"></span>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <span data-tooltip title="Completed"></span>
+                                Step Name Here
+                            </li>
+                            <li class="active">
+                                <span data-tooltip title="Completed"></span>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <span data-tooltip title="Completed"></span>
+                                Step Name Here
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="padding-30-v">
+                        <ul class="steps-bar icons-sm">
+                            <li>
+                                <svg class="icon margin-3-r"><use href="#check-circle"/></svg>
+                                Step Name Here
+                            </li>
+                            <li class="active">
+                                <svg class="icon margin-3-r"><use href="#check-circle"/></svg>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <svg class="icon margin-3-r"><use href="#check-circle"/></svg>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <svg class="icon margin-3-r"><use href="#check-circle"/></svg>
+                                Step Name Here
+                            </li>
+                        </ul>
+                        <span class="sp-15"></span>
+                        <ul class="steps-bar steps-icon">
+                            <li>
+                                <svg class="icon" data-tooltip title="Completed"><use href="#check-circle-fill"/></svg>
+                                Step Name Here
+                            </li>
+                            <li class="active">
+                                <svg class="icon" data-tooltip title="Completed"><use href="#check-circle-fill"/></svg>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <svg class="icon" data-tooltip title="Completed"><use href="#check-circle-fill"/></svg>
+                                Step Name Here
+                            </li>
+                            <li>
+                                <svg class="icon" data-tooltip title="Completed"><use href="#check-circle-fill"/></svg>
+                                Step Name Here
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
