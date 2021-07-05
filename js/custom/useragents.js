d30262af7a30d7caaff7eccd95db1ed440e13a02
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2021-07-05 16:09:44 +0300

    Bug fix for timing.

--- js/custom/useragents.js
+++ js/custom/useragents.js
@@ -1,11 +1,13 @@
-/*globals document, selector, events, useragents */
+/*globals document, selector, events, useragents, setTimeout */
 
 events.onload(function () {
 
-    var results = selector(document)[0].getAttribute('class');
-    selector('.results')[0].innerHTML = '&lt;html class="' + results + '"&gt;';
+    setTimeout(function () {
 
-    selector('.variables')[0].innerHTML = 'useragents.userLang: ' + useragents.userLang + '<br>' +
+        var results = selector(document)[0].getAttribute('class');
+        selector('.results')[0].innerHTML = '&lt;html class="' + results + '"&gt;';
+
+        selector('.variables')[0].innerHTML = 'useragents.userLang: ' + useragents.userLang + '<br>' +
         'useragents.ie: ' + useragents.ie + '<br>' +
         'useragents.edge: ' + useragents.edge + '<br>' +
         'useragents.edg: ' + useragents.edg + ' (Chromium Edge)<br>' +
@@ -14,5 +16,7 @@ events.onload(function () {
         'useragents.android: ' + useragents.android + '<br>' +
         'useragents.androidOld: ' + useragents.androidOld + '<br>' +
         'useragents.nativeBrowser: ' + useragents.nativeBrowser;
+
+    }, 300);
 
 });
