39e7e2dd9391959dee6b54d78363811cae9f4e69
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2021-08-03 14:10:53 +0300

    Updated dropdown open timer from 50 to 150.

--- js/dropdown.js
+++ js/dropdown.js
@@ -73,7 +73,7 @@ var dropdown = {};
 
                 dropdownOpenTimer = setTimeout(function () {
                     events.addClass(parent, 'open-ease');
-                }, 50);
+                }, 150);
 
                 offset = parent.getBoundingClientRect();
                 list = selector('.content', parent)[0];
