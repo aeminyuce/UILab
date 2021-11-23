33bc286c623711bb4dc8d286b2fd4296f85f7fef
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2021-11-23 10:59:39 +0300

    Improved fonts and spacings.

--- docs/modal.php
+++ docs/modal.php
@@ -56,8 +56,8 @@
             <div class="ui-col-6 ui-no-p-v">
 
                 <i class="ui-color-black-50 ui-m-5-b ui-block">Open modal window with id.</i>
-                <pre class="ui-pre ui-round ui-m-5-b">ui.modal.open({<br>    source: '#modalExample'<br>});</pre>
-                <button class="ui-btn ui-btn-sm ui-round ui-fill-dark-100 ui-ease-btn" onclick="ui.modal.open({source: '#modalExample'});">Run Code</button>
+                <pre class="ui-pre ui-round ui-m-5-b">ui.modal.open({<br>    source: '#modal-example'<br>});</pre>
+                <button class="ui-btn ui-btn-sm ui-round ui-fill-dark-100 ui-ease-btn" onclick="ui.modal.open({source: '#modal-example'});">Run Code</button>
 
             </div>
             <div class="ui-col-6 ui-no-p-t">
@@ -139,8 +139,8 @@
                 <span class="ui-sp-15"></span>
 
                 <i class="ui-color-black-50 ui-m-5-b ui-block">Google map embed link with header and footer:</i>
-                <pre class="ui-pre ui-round ui-m-5-b">ui.modal.open({<br>    source: '#modal-example02'<br>    size: 'fullscreen'<br>});</pre>
-                <button class="ui-btn ui-btn-sm ui-round ui-fill-dark-100 ui-ease-btn" onclick="ui.modal.open({source: '#modalExample02', size: 'fullscreen'});">Run Code</button>
+                <pre class="ui-pre ui-round ui-m-5-b">ui.modal.open({<br>    source: '#modal-example'<br>    size: 'fullscreen'<br>});</pre>
+                <button class="ui-btn ui-btn-sm ui-round ui-fill-dark-100 ui-ease-btn" onclick="ui.modal.open({source: '#modal-example', size: 'fullscreen'});">Run Code</button>
 
             </div>
 
@@ -200,7 +200,7 @@
 </main>
 
 <!-- modal sources -->
-<div id="modalExample" class="ui-modal modal-example ui-theme-base">
+<div id="modal-example" class="ui-modal modal-example ui-theme-base">
     <div class="ui-modal-header ui-fill-dark-100">
         <h4 class="ui-h4">Modal Header</h4>
         <div class="ui-modal-buttons ui-ease-1st-btn">
