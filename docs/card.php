334c53b9a13698cd7f95cca12d905f5200e11fa0
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2021-11-29 17:32:55 +0300

    Updated cards.

--- docs/card.php
+++ docs/card.php
@@ -5,17 +5,17 @@
 
                 <h3 class="ui-h3">Card Examples</h4>
                 <div class="ui-row ui-m-30-b">
-                    <div class="ui-col-static ui-sm-fluid">
+                    <div class="ui-col-static">
 
                         <div class="ui-row">
                             <div class="ui-col-12">
 
-                                <div class="ui-card ui-p-30 ui-round ui-shadow">
+                                <div class="ui-card ui-p-30 ui-round ui-shadow-lg">
                                     <h3 class="ui-h3 ui-m-15-b">Title</h3>
                                     <p class="ui-font-lead ui-font-readable">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam.
                                     </p>
-                                    <img class="ui-img-sm-fluid ui-highlight ui-m-auto" src="img/image_04.jpg" height="260" alt="">
+                                    <img class="ui-img-md-fluid ui-highlight ui-m-auto" src="img/image_04.jpg" height="308" alt="">
                                     <p class="ui-font-readable">
                                         In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
                                     </p>
@@ -25,7 +25,7 @@
                         </div>
                         <div class="ui-col-350 ui-p-10">
 
-                            <div class="ui-card ui-round ui-shadow ui-theme-sub">
+                            <div class="ui-card ui-round ui-shadow-lg ui-theme-sub">
                                 <div class="ui-card-side">
                                     <img class="ui-img-fluid ui-round-t" src="img/image_06.jpg" alt="">
                                 </div>
@@ -33,9 +33,24 @@
                                     <h3 class="ui-h3">Card title</h3>
                                     <p class="ui-highlight">
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat felis ut nibh malesuada, id imperdiet libero molestie.
-                                        <br><br>
-                                        Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor.
                                     </p>
+                                </div>
+                                <div class="ui-card-side">
+                                    <div class="ui-listgroup">
+                                        <ul class="ui-ease-listgroup">
+                                            <li>
+                                                <a class="ui-p-15-l" href="#">Card Link</a>
+                                            </li>
+                                            <li>
+                                                <a class="ui-p-15-l" href="#">Card Link</a>
+                                            </li>
+                                            <li>
+                                                <a class="ui-p-15-l" href="#">Card Link</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="ui-card-side ui-p-15">
                                     <a class="ui-btn ui-block ui-round ui-fill-dark-100 ui-ease-btn" href="#">Button</a>
                                 </div>
                             </div>
@@ -48,30 +63,34 @@
                 <h3 class="ui-h3">Card Sides</h3>
                 <div class="ui-p-30-b">
 
-                    <div class="ui-card ui-round ui-shadow">
-                        <div class="ui-card-side ui-p-15 ui-border-b">
-                            <h4 class="ui-h4"><svg class="ui-icon ui-icon-lg"><use href="#user"/></svg>Card Title</h4>
+                    <div class="ui-card ui-round ui-shadow-lg ui-theme-gray">
+                        <div class="ui-card-side ui-p-15">
+                            <h4 class="ui-h4">
+                                <svg class="ui-icon ui-icon-lg"><use href="#user"/></svg>
+                                Card Title
+                            </h4>
                         </div>
-                        <div class="ui-card-side ui-p-15 ui-font-readable">
-                            <img class="ui-img-sm-fluid ui-highlight ui-m-auto" src="img/image_02.jpg" height="460" alt="">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
-                            <br><br>
-                            In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
+                        <div class="ui-card-side ui-align-c ui-p-30 ui-m-1-b ui-fill-light-100">
+                            <img class="ui-img-sm-fluid" src="img/image_02.jpg" height="320" alt="">
+                            <span class="ui-sp-10"></span>
+                            <i class="ui-color-black-50">Lorem ipsum dolor sit amet</i>
                         </div>
-                        <div class="ui-card-side ui-p-15 ui-theme-gray ui-fill-light-100">
+                        <div class="ui-card-side ui-p-15 ui-fill-light-300">
                             <h4 class="ui-h4">Separated Title</h4>
                         </div>
                         <div class="ui-card-side ui-p-15 ui-font-readable">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat felis ut nibh malesuada, id imperdiet libero molestie. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
+                            <br><br>
+                            In feugiat felis ut nibh malesuada, id imperdiet libero molestie. Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
                             <br><br>
                             Sed ut ipsum quis nisi congue aliquet id at diam. Proin nec fermentum odio. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor. Morbi quis metus vitae ligula porta rhoncus sit amet ac tellus. In hac habitasse platea dictumst. Donec at pulvinar libero. Mauris mattis leo mi, venenatis semper risus pulvinar ac.
                         </div>
-                        <div class="ui-card-side ui-p-15 ui-theme-gray ui-fill-light-100">
+                        <div class="ui-card-side ui-color-black-50 ui-p-15 ui-border-t">
                             Card Footer
                         </div>
                     </div>
 
-                    <div class="ui-card ui-round ui-shadow">
+                    <div class="ui-card ui-round ui-shadow-lg">
                         <div class="ui-card-side ui-p-15 ui-border-b">
                             <h4 class="ui-h4"><svg class="ui-icon ui-icon-lg"><use href="#lab"/></svg>Card Title</h4>
                         </div>
@@ -110,7 +129,7 @@
                         </div>
                     </div>
 
-                    <div class="ui-card ui-round ui-shadow">
+                    <div class="ui-card ui-round ui-shadow-lg">
                         <div class="ui-card-side ui-p-15 ui-border-b">
                             <h4 class="ui-h4"><svg class="ui-icon ui-icon-lg"><use href="#check-badge"/></svg>Card Title</h4>
                         </div>
@@ -237,7 +256,7 @@
                 <h3 class="ui-h3">Card Messages</h3>
                 <div class="ui-p-30-b">
 
-                    <div class="ui-card-success ui-p-15 ui-round ui-shadow">
+                    <div class="ui-card-success ui-p-15 ui-round ui-shadow-lg">
                         <button class="ui-card-close ui-ease-btn">
                             <svg class="ui-icon"><use href="#remove"/></svg>
                         </button>
@@ -248,7 +267,7 @@
                         <p class="ui-font-16">Lorem ipsum dolor sit amet. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor.</p>
                     </div>
 
-                    <div class="ui-card-danger ui-p-15 ui-round ui-shadow">
+                    <div class="ui-card-danger ui-p-15 ui-round ui-shadow-lg">
                         <button class="ui-card-close ui-ease-btn">
                             <svg class="ui-icon"><use href="#remove"/></svg>
                         </button>
@@ -259,7 +278,7 @@
                         <p class="ui-font-16">Lorem ipsum dolor sit amet. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor.</p>
                     </div>
 
-                    <div class="ui-card-warning ui-p-15 ui-round">
+                    <div class="ui-card-warning ui-p-15 ui-round ui-shadow-lg">
                         <button class="ui-card-close ui-ease-btn">
                             <svg class="ui-icon"><use href="#remove"/></svg>
                         </button>
@@ -270,7 +289,7 @@
                         <p class="ui-font-16">Lorem ipsum dolor sit amet. Pellentesque neque enim, finibus sed malesuada eget, vestibulum id tortor.</p>
                     </div>
 
-                    <div class="ui-card-info ui-p-15 ui-round ui-shadow">
+                    <div class="ui-card-info ui-p-15 ui-round ui-shadow-lg">
                         <button class="ui-card-close ui-ease-btn">
                             <svg class="ui-icon"><use href="#remove"/></svg>
                         </button>
@@ -302,7 +321,7 @@
                     </div>
                 </div>
 
-                <div class="ui-card-warning ui-p-30 ui-round ui-theme-green ui-fill-light-100 ui-stroke">
+                <div class="ui-card-warning ui-p-30 ui-round ui-theme-green ui-fill-light-100 ui-stroke ui-shadow-lg">
                     <h4 class="ui-h4">
                         <svg class="ui-icon ui-icon-xl ui-text"><use href="#help-fill"/></svg>
                         Message Title
@@ -315,7 +334,7 @@
                     </a>
                 </div>
 
-                <div class="ui-card-warning ui-p-30 ui-round ui-theme-yellow ui-fill-light-100 ui-stroke">
+                <div class="ui-card-warning ui-p-30 ui-round ui-theme-yellow ui-fill-light-100 ui-stroke ui-shadow-lg">
                     <h4 class="ui-h4">
                         <svg class="ui-icon ui-icon-xl ui-text"><use href="#help-fill"/></svg>
                         Message Title
@@ -328,7 +347,7 @@
                     </a>
                 </div>
 
-                <div class="ui-card-warning ui-p-30 ui-round ui-theme-red ui-fill-light-100 ui-stroke">
+                <div class="ui-card-warning ui-p-30 ui-round ui-theme-red ui-fill-light-100 ui-stroke ui-shadow-lg">
                     <h4 class="ui-h4">
                         <svg class="ui-icon ui-icon-xl ui-text"><use href="#help-fill"/></svg>
                         Message Title
