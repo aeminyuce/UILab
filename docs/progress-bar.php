4a55b18a67e3266a0024cad5a4aaf770215aa137
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2021-11-30 14:26:13 +0300

    Updated progress bars.

--- docs/progress-bar.php
+++ docs/progress-bar.php
@@ -5,21 +5,56 @@
 
                 <h3 class="ui-h3">Progress Bar Examples</h3>
                 <div class="ui-card ui-highlight ui-p-30 ui-round ui-shadow-lg">
-                    <div class="ui-progress-bar ui-progress-sm ui-m-15 ui-round">
+
+                    <div class="ui-progress-bar ui-progress-sm ui-m-15">
                         <span style="width: 30%;"></span>
                     </div>
 
-                    <div class="ui-progress-bar ui-m-15 ui-round">
+                    <div class="ui-progress-bar ui-m-15">
                         <span style="width: 60%;"></span>
                     </div>
 
-                    <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span style="width: 80%;"></span>
+                    <div class="ui-progress-bar ui-progress-lg ui-m-15">
+                        <span style="width: 80%;">80%</span>
                     </div>
+
+                    <span class="ui-sp-15"></span>
+
+                    <div class="ui-theme-yellow">
+                        <div class="ui-progress-bar ui-progress-sm ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 30%;"></span>
+                        </div>
+
+                        <div class="ui-progress-bar ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 60%;"></span>
+                        </div>
+
+                        <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 80%;">80%</span>
+                        </div>
+                    </div>
+
+                    <span class="ui-sp-15"></span>
+
+                    <div class="ui-theme-orange">
+                        <div class="ui-progress-bar ui-progress-sm ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 30%;"></span>
+                        </div>
+
+                        <div class="ui-progress-bar ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 60%;"></span>
+                        </div>
+
+                        <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-circle">
+                            <span class="ui-fill-dark-100" style="width: 80%;">80%</span>
+                        </div>
+                    </div>
+
                 </div>
 
                 <h5 class="ui-h5 ui-font-bold ui-m-15-b">Separated Progress Bars:</h5>
                 <div class="ui-card ui-highlight ui-p-30 ui-round ui-shadow-lg">
+
                     <div class="ui-progress-bar ui-progress-sm ui-m-15 ui-round">
                         <span class="ui-theme-green ui-fill-dark-100" style="width: 45%;"></span>
                         <span class="ui-theme-yellow ui-fill-dark-100" style="width: 15%;"></span>
@@ -31,47 +66,51 @@
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-base ui-fill-dark-100" style="width: 40%;"></span>
-                        <span class="ui-theme-sub ui-fill-dark-100" style="width: 45%;"></span>
+                        <span class="ui-theme-base ui-fill-dark-100" style="width: 40%;">40%</span>
+                        <span class="ui-theme-sub ui-fill-dark-100" style="width: 45%;">45%</span>
                     </div>
+
                 </div>
 
                 <h5 class="ui-h5 ui-font-bold ui-m-15-b">Striped Progress Bars:</h5>
                 <div class="ui-card ui-highlight ui-p-30 ui-round ui-shadow-lg">
+
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-yellow ui-fill-light-300 ui-stripe-light ui-stripe-sm" style="width: 75%;"></span>
+                        <span class="ui-theme-yellow ui-fill-light-300 ui-stripe-light ui-stripe-sm" style="width: 75%;">75%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-yellow ui-fill-dark-100 ui-stripe-dark ui-stripe-sm" style="width: 75%;"></span>
+                        <span class="ui-theme-yellow ui-fill-dark-100 ui-stripe-dark ui-stripe-sm" style="width: 75%;">75%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-orange ui-fill-light-300 ui-stripe-light" style="width: 40%;"></span>
+                        <span class="ui-theme-orange ui-fill-light-300 ui-stripe-light" style="width: 40%;">40%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-orange ui-fill-dark-100 ui-stripe-dark" style="width: 40%;"></span>
+                        <span class="ui-theme-orange ui-fill-dark-100 ui-stripe-dark" style="width: 40%;">40%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-red ui-fill-light-300 ui-stripe-light ui-stripe-lg" style="width: 60%;"></span>
+                        <span class="ui-theme-red ui-fill-light-300 ui-stripe-light ui-stripe-lg" style="width: 60%;">60%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-red ui-fill-dark-100 ui-stripe-dark ui-stripe-lg" style="width: 60%;"></span>
+                        <span class="ui-theme-red ui-fill-dark-100 ui-stripe-dark ui-stripe-lg" style="width: 60%;">60%</span>
                     </div>
                 </div>
 
                 <h5 class="ui-h5 ui-font-bold ui-m-15-b">Striped &amp; Animated Progress Bars:</h5>
                 <div class="ui-card ui-highlight ui-p-30 ui-round ui-shadow-lg">
+
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-sub ui-fill-light-300 ui-stripe-light ui-animate-progress" style="width: 80%;"></span>
+                        <span class="ui-theme-sub ui-fill-light-300 ui-stripe-light ui-animate-progress" style="width: 50%;">50%</span>
                     </div>
 
                     <div class="ui-progress-bar ui-progress-lg ui-m-15 ui-round">
-                        <span class="ui-theme-sub ui-fill-dark-100 ui-stripe-dark ui-animate-progress" style="width: 80%;"></span>
+                        <span class="ui-theme-sub ui-fill-dark-100 ui-stripe-dark ui-animate-progress" style="width: 80%;">80%</span>
                     </div>
+
                 </div>
 
             </div>
