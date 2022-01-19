81079759825287ef9128a25bc2548cf6b2757283
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2022-01-19 17:58:21 +0300

    Updated addclass test.

--- tests/core/addclass.test.js
+++ tests/core/addclass.test.js
@@ -18,10 +18,10 @@ global.document = window.document;
 const el = global.document.querySelector("div");
 
 // test js file
-import { default as addClass } from './../../js/core/addClass.js';
+import * as ui from './../../dist/js/ui.js';
 
 const classname = 'test-classname';
-addClass(el, classname);
+ui.addClass(el, classname);
 
 // asserts
 describe("Core AddClass", () => {
