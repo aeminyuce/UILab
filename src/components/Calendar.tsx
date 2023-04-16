87ae4e040a4e93adebbe3c5ec07cc78a5c57637b
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2023-04-17 01:46:33 +0300

    Improved calendar react component.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -8,7 +8,7 @@ import Form from '@components/Form';
 const icon_arrow_left = '<path d="m4.223 124.175 120-119.955a11 11 0 0 1 15.556 0 11 11 0 0 1 0 15.556L38.519 121H251a11 11 0 0 1 11 11 11 11 0 0 1-11 11H38.608l101.168 101.131a11 11 0 0 1 0 15.556 11 11 0 0 1-15.556 0l-120-119.955a11 11 0 0 1-3.223-7.781 11 11 0 0 1 3.226-7.776Z" />';
 const icon_arrow_right = '<path d="M12 121h212.48L123.223 19.78a11 11 0 0 1 0-15.556 11 11 0 0 1 15.556 0l120 119.955a11 11 0 0 1 3.223 7.781 11 11 0 0 1-3.223 7.779l-120 119.955a11 11 0 0 1-15.556 0A10.969 10.969 0 0 1 120 251.91a10.961 10.961 0 0 1 3.223-7.779L224.392 143H12a11 11 0 0 1-11-11 11 11 0 0 1 11-11Z" />';
 const icon_angle_left = '<path d="M61 131.955a11 11 0 0 1 3.223-7.78l120-119.955a11 11 0 0 1 15.556 0 11 11 0 0 1 0 15.556L87.559 131.955l112.218 112.176a11 11 0 0 1 0 15.556 11 11 0 0 1-15.556 0l-120-119.955A11 11 0 0 1 61 131.955Z" />';
-const icon_calendar = '<path d="M12 263a11 11 0 0 1-11-11V28a11 11 0 0 1 11-11h37v-5A11 11 0 0 1 60 1a11 11 0 0 1 11 11v5h122v-5a11 11 0 0 1 11-11 11 11 0 0 1 11 11v5h37a11 11 0 0 1 11 11v224a11 11 0 0 1-11 11Zm11-22h218V95H23ZM241 73V39h-26v5a11 11 0 0 1-11 11 11 11 0 0 1-11-11v-5H71v5a11 11 0 0 1-11 11 11 11 0 0 1-11-11v-5H23v34Z" />';
+const icon_calendar = 'M12 263a11 11 0 0 1-11-11V28a11 11 0 0 1 11-11h37v-5A11 11 0 0 1 60 1a11 11 0 0 1 11 11v5h122v-5a11 11 0 0 1 11-11 11 11 0 0 1 11 11v5h37a11 11 0 0 1 11 11v224a11 11 0 0 1-11 11Zm11-22h218V95H23ZM241 73V39h-26v5a11 11 0 0 1-11 11 11 11 0 0 1-11-11v-5H71v5a11 11 0 0 1-11 11 11 11 0 0 1-11-11v-5H23v34Z';
 
 import '@less/modules/calendar';
 import '@js/modules/calendar';
@@ -36,8 +36,21 @@ const Calendar = function () {
 
 interface CalendarPickerProps {
 
-    placeholder?: string,
+    onChange?: React.ReactEventHandler,
+    onInput?: React.ReactEventHandler,
+    onBlur?: React.ReactEventHandler,
 
+    name?: string,
+    tabIndex?: number,
+    value?: any,
+    defaultValue?: any,
+    placeholder?: string,
+    disabled?: boolean,
+    autoComplete?: 'on' | 'off' | 'username' | 'current-password' | string,
+
+    myRef?: any,
+
+    id?: any,
     className?: string,
     style?: any,
 
@@ -45,7 +58,7 @@ interface CalendarPickerProps {
 
 const CalendarPicker = function (
 
-    { placeholder, className, style }:CalendarPickerProps) {
+    { onChange, onInput, onBlur, name, tabIndex, value, defaultValue, placeholder, disabled, autoComplete, myRef, id, className, style }:CalendarPickerProps) {
 
         useEffect(() => {
 
@@ -65,8 +78,10 @@ const CalendarPicker = function (
         const classes = 'ui-calendar-picker ui-form-icon-l' + setClassName;
 
         return (
-            <Form.Input placeholder={placeholder} className={classes} style={style}>
-                <Icon src={icon_calendar} />
+            <Form.Input id={id} myRef={myRef} name={name} tabIndex={tabIndex} value={value} defaultValue={defaultValue} placeholder={placeholder}
+                 autoComplete={autoComplete} className={classes} style={style} disabled={disabled}
+                 onChange={onChange} onInput={onInput} onBlur={onBlur}>
+                    <Icon path src={icon_calendar} />
             </Form.Input>
         );
     }
