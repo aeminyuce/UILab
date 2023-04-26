ebfc57ad28529cdd626d4d1cd6e185021c0c1ec9
emin.yuce@keyyazilim.com <emin.yuce@keyyazilim.com>
2023-04-26 11:24:27 +0300

    Documentation updates.

--- docs/xhr/ajax-modal.php
+++ docs/xhr/ajax-modal.php
@@ -5,7 +5,29 @@
     </div>
 </div>
 <div class="ui-modal-container ui-font-16 ui-p-30">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec tincidunt eros. Vivamus a est id dui ultricies congue vel et tellus. Nam ac velit mi. Donec vitae suscipit enim, nec consectetur nisl. Quisque ac posuere lectus. Morbi quam diam, cursus et metus at, lobortis iaculis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus lectus nibh, non elementum justo facilisis a. Aliquam erat volutpat. Vestibulum auctor et diam eget eleifend. Donec blandit ligula aliquam mi lacinia lacinia. Cras ac bibendum urna. Suspendisse posuere ullamcorper enim, non varius diam mattis vehicula. Donec nec lacinia diam.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec tincidunt eros. Vivamus a est id dui ultricies congue vel et tellus. Nam ac velit mi. Donec vitae suscipit enim, nec consectetur nisl.
+    <span class="ui-sp-10"></span>
+        <div class="ui-color-black-50 ui-bold ui-m-5-b">Grid in Modal Example:</div>
+        <div class="ui-row ui-no-fluid ui-m-5-b">
+            <div class="ui-col-6">
+                <div class="ui-border ui-p-5">ui-col-6</div>
+            </div>
+            <div class="ui-col-6">
+                <div class="ui-border ui-p-5">ui-col-6</div>
+            </div>
+        </div>
+        <div class="ui-color-black-50 ui-bold ui-m-5-b">Fixed Grid in Modal Example:</div>
+        <div class="ui-fixed ui-m-10-b">
+            <div class="ui-row ui-no-fluid">
+                <div class="ui-col-6">
+                    <div class="ui-border ui-p-5">ui-col-6</div>
+                </div>
+                <div class="ui-col-6">
+                    <div class="ui-border ui-p-5">ui-col-6</div>
+                </div>
+            </div>
+        </div>
+    Quisque ac posuere lectus. Morbi quam diam, cursus et metus at, lobortis iaculis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus lectus nibh, non elementum justo facilisis a. Aliquam erat volutpat. Vestibulum auctor et diam eget eleifend. Donec blandit ligula aliquam mi lacinia lacinia. Cras ac bibendum urna. Suspendisse posuere ullamcorper enim, non varius diam mattis vehicula. Donec nec lacinia diam.
     <img class="ui-img-fluid ui-m-30-v ui-round ui-m-auto ui-block ui-shadow-lg" src="../public/img/image_05.jpg" style="max-width: 700px" alt="">
     Pellentesque congue eget elit vitae rutrum. Morbi bibendum dignissim convallis. Etiam lacinia quam et ipsum convallis, a congue metus sagittis. Nunc posuere iaculis nulla, nec rutrum neque condimentum at. Donec nec elementum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor libero sem, sed bibendum elit viverra vitae.
     <br><br>
