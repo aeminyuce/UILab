0e987b865e142622b775a44f85615f6787d8480e
Emin Yuce <yuce_ahmet@hotmail.com>
2024-06-19 21:11:05 +0300

    Changed file structure for npmjs.

--- src/utils/PageRoutes.ts
+++ src/utils/PageRoutes.ts
@@ -1,11 +1,4 @@
 import { lazy } from 'react';
 
-export const Home = lazy(() => import(
-    /* webpackChunkName: "Home" */
-    "../Home"
-));
-
-export const Calendar = lazy(() => import(
-    /* webpackChunkName: "Calendar" */
-    "../Calendar"
-));
+export const Home = lazy(() => import( /* webpackChunkName: "Home" */ "../Home" ));
+export const Calendar = lazy(() => import( /* webpackChunkName: "Calendar" */ "../Calendar" ));
