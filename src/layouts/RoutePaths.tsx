41f32af37efa264c0b60f9f2e7558d50aec2019d
Emin Yuce <yuce_ahmet@hotmail.com>
2024-09-08 16:39:42 +0300

    Route paths file created.

---
+++ src/layouts/RoutePaths.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// layouts
+import PageTitle from '@layouts/PageTitle';
+
+// utils
+import {
+    Home,
+    Calendar
+} from '@utils/PageRoutes';
+
+export default function RoutePaths() {
+    return (
+        <Routes>
+            <Route path='/*' element={
+                <PageTitle title='Home'><Home /></PageTitle>
+            } />
+            <Route path='calendar' element={
+                <PageTitle title='Calendar'><Calendar /></PageTitle>
+            } />
+        </Routes>
+    );
+}
