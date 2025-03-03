71bd08fcede2d3a1f27f2c41526986a881fa70b4
Emin Yuce <yuce_ahmet@hotmail.com>
2025-03-03 21:55:28 +0300

    Removed hashRouter.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
 // layouts
@@ -15,14 +15,8 @@ import App from './App';
 const root = createRoot(document.getElementById('app'));
 root.render(
     <StoreProvider>
-        {process.env.NODE_ENV === 'production' ?
-            <HashRouter>
-                <App />
-            </HashRouter>
-            :
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        }
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StoreProvider>
 );
